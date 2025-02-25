214323354e4c81e9b50b4e80e746ee4b67cc4597
Concepcion Orellana <oc.concepcionorellana@gmail.com>
2025-02-25 09:57:05 -0600

    chore: clean code

--- app/(tabs)/bookmark.tsx
+++ app/(tabs)/bookmark.tsx
@@ -4,8 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Bookmark() {
 	return (
-		<SafeAreaView className="px-4 my-6 bg-primary h-full">
-			<Text className="text-2xl text-white font-psemibold">Bookmark</Text>
+		<SafeAreaView className="bg-primary h-full">
+			<Text className="text-2xl text-white font-semibold">Bookmark</Text>
 		</SafeAreaView>
 	);
 }
